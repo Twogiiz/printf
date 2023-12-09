8b8e0ba440c2db1e15fe51f31f406017fce4fc40
Twogiiz <goodwillmonyai@gmail.com>
2023-12-08 18:36:23 -0800

    _print.c fix

--- functions.c
+++ functions.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/************************* PRINT CHAR *************************/
+////****\\\\\\PRINT CHAR//////*******\\\\
 
 /**
  * print_char - Prints a char
@@ -19,7 +19,7 @@ int print_char(va_list types, char buffer[],
 
 	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
-/************************* PRINT A STRING *************************/
+/*******\ PRINT A STRING ******//
 /**
  * print_string - Prints a string
  * @types: List a of arguments
@@ -74,7 +74,8 @@ int print_string(va_list types, char buffer[],
 
 	return (write(1, str, length));
 }
-/************************* PRINT PERCENT SIGN *************************/
+////*******\\\PRINT PERCENT SIGN/////
+
 /**
  * print_percent - Prints a percent sign
  * @types: Lista of arguments
@@ -97,7 +98,7 @@ int print_percent(va_list types, char buffer[],
 	return (write(1, "%%", 1));
 }
 
-/************************* PRINT INT *************************/
+/*******//// PRINT INT //////
 /**
  * print_int - Print int
  * @types: Lista of arguments
